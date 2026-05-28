0409c01f10dcfb7810fd204793c31cdd8b40c0fb
nazar.khimin <khimin1719@gmail.com>
2026-05-28 23:15:16 +0300

    fix: stamp 0001 if schema exists without alembic_version

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -e
-alembic upgrade head
+# If schema exists but alembic_version is missing/wrong, stamp 0001 first then upgrade.
+# This handles restored databases that predate alembic tracking.
+alembic upgrade head || (alembic stamp 0001 && alembic upgrade head)
 exec gunicorn finance_api.main:app -c gunicorn.conf.py
